e3eb456c20ec8f7acf014bddc674b0b660671713
soheil-mx <soheilzolfaghari78@gmail.com>
2025-05-03 13:43:01 +0330

    Data Updated

--- lib/data/consts.dart
+++ lib/data/consts.dart
@@ -1,4 +1,29 @@
+import 'package:flutter/material.dart';
+
 class KeyConsts {
   static const String themeModeKey = "themeModeKey";
   static const String firstOpenKey = "firstOpenKey";
+}
+
+class AppColor {
+  static const List<Color> availableColors = [
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.purple,
+    Colors.teal,
+    Colors.yellow,
+    Colors.indigo,
+    Colors.brown,
+    Colors.pink,
+    Colors.amber,
+    Colors.cyan,
+    Colors.lime,
+    Colors.deepOrange,
+    Colors.lightBlue,
+    Colors.lightGreen,
+    Colors.purpleAccent,
+    Colors.deepPurple,
+  ];
 }
--- lib/data/notifiers.dart
+++ lib/data/notifiers.dart
@@ -1,10 +1,16 @@
+import 'package:back_in_line/data/reminder/reminder.dart';
 import 'package:flutter/material.dart';
 
 // Dark Mode
 ValueNotifier<bool> isDarkModeNotifier = ValueNotifier(true);
 
 // Welcome Page
-ValueNotifier<int> currentWelcomePage = ValueNotifier<int>(0);
-PageController welcomePageViewController = PageController(
-  viewportFraction: 1.0,
+ValueNotifier<int> currentWelcomePageNotifier = ValueNotifier<int>(0);
+
+// Reminder
+ValueNotifier<List<Reminder>> remindersNotifier = ValueNotifier<List<Reminder>>(
+  [],
+);
+final ValueNotifier<int?> selectedReminderIndexNotifier = ValueNotifier<int?>(
+  null,
 );
